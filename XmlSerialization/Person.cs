bb200b84cf71fe0d4ea0c9041c6af92b28d6a22b
David Navasardyan <david.navasardyan@hotmail.com>
2019-01-31 09:20:11 +0400

    bject serializ

---
+++ XmlSerialization/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlSerialization
+{
+        //The class to be serialized must have a standard constructor without parameters.
+        //Also only open members are subject to serialization.If the class has fields or properties 
+        //with the private modifier, then they will be ignored during serialization.
+    [Serializable]
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        public Person() { } //Defaoult constructor without parameters
+        public Person(string personName, int personAge)
+        {
+            this.Name = personName;
+            this.Age = personAge;
+        }
+
+        public override string ToString() => string.Format($"Person Name: {Name}\nPerson Age: {Age}");
+    }
+}
--- XmlSerialization/Program.cs
+++ XmlSerialization/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace XmlSerialization
 {
@@ -10,6 +12,18 @@ namespace XmlSerialization
     {
         static void Main(string[] args)
         {
+            Person person = new Person("David", 42);
+            Console.WriteLine($"Object created");
+
+            // pass the class type to the constructor
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+            // get the stream where we will write the serialized object
+            using(FileStream fileStream = new FileStream("person.xml", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fileStream, person);
+                Console.WriteLine($"Object serialized");
+            }
         }
     }
 }
